f2c5352d9df2d634fa186eec2291787668c1a7c1
warizz yutanan <warizz.yutanan@gmail.com>
2017-03-03 15:56:09 +0700

    fix github page issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Perf from 'react-addons-perf';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers/index';
@@ -20,7 +20,7 @@ const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path="/sign-in" component={SignInPage} />
       <Route component={Layout} onEnter={lib.initApplicationState(store)}>
         <IndexRoute component={BetPage} />
