a59805ac3fe5435f295401ef4e35f36e6dc10477
warizz <warizz.yutanan@gmail.com>
2017-03-26 14:40:07 +0700

    move fb function inside

--- src/index.js
+++ src/index.js
@@ -18,6 +18,27 @@ import lib from './constants/lib';
 window.Perf = Perf;
 const store = createStore(reducer);
 
+window.fbAsyncInit = () => {
+  window.FB.init({
+    appId: process.env.REACT_APP_FB_APP_ID,
+    cookie: true,
+    xfbml: true,
+    version: 'v2.8',
+  });
+  window.FB.AppEvents.logPageView();
+};
+
+((d, s, id) => {
+  const fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  const js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>
