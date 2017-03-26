a1a7be7e7927ad9ea479617352c9f77b99d960c4
warizz <warizz.yutanan@gmail.com>
2017-03-26 14:18:04 +0700

    remove firebase thingies

--- src/services/data.js
+++ src/services/data.js
@@ -22,102 +22,31 @@ function cleanPeriod(obj) {
 }
 
 function getPeriods(username, callback) {
-  firebase
-    .database()
-    .ref('periods')
-    .orderByKey()
-    .limitToLast(10)
-    .on('value', (snapshot) => {
-      const val = snapshot.val();
-      if (!val) {
-        callback([]);
-        return;
-      }
-      const periods = Object.keys(val)
-        .map(key => val[key])
-        .map(cleanPeriod)
-        .sort((a, b) => {
-          if (a.createdAt < b.createdAt) return 1;
-          if (a.createdAt > b.createdAt) return -1;
-          return 0;
-        });
-      if (typeof callback === 'function') {
-        callback(periods);
-      }
-    });
+
 }
 
 function openPeriod(endDate) {
-  const period = {
-    createdAt: new Date().getTime(),
-    endDate: endDate.getTime(),
-    id: new Date().valueOf().toString(),
-    open: true,
-  };
-  firebase
-    .database()
-    .ref()
-    .child('periods')
-    .child(period.id)
-    .set(period);
+
 }
 
 function closePeriod(period) {
-  firebase
-    .database()
-    .ref()
-    .child('periods')
-    .child(period.id)
-    .update({ open: false });
+
 }
 
 function setPaid(periodId, bets, paid) {
-  const updatedBets = {};
-  bets.forEach((bet) => {
-    updatedBets[`periods/${periodId}/bets/${bet.id}/paid`] = paid;
-  });
-  firebase
-    .database()
-    .ref()
-    .update(updatedBets);
+
 }
 
 function insertBet(periodId, bet) {
-  const updated = Object.assign(bet, {
-    id: bet.id || new Date().valueOf().toString(),
-    createdAt: new Date().getTime(),
-  });
-  firebase
-    .database()
-    .ref()
-    .child('periods')
-    .child(periodId)
-    .child('bets')
-    .child(updated.id)
-    .set(updated);
+
 }
 
 function deleteBet(periodId, id) {
-  firebase
-    .database()
-    .ref()
-    .child('periods')
-    .child(periodId)
-    .child('bets')
-    .child(id)
-    .remove();
+
 }
 
 function saveResult(periodId, result) {
-  return new Promise((resolve) => {
-    firebase
-      .database()
-      .ref()
-      .child('periods')
-      .child(periodId)
-      .update({ result });
-    resolve(true);
-  });
+
 }
 
 export default {
