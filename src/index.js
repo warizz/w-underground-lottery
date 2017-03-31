f5fc963ade9c14e7a5ef1b3af03a10f206e785cc
warizz yutanan <warizz.yutanan@gmail.com>
2017-03-31 13:18:17 +0700

    move facebook's sdk to vendors/

--- src/index.js
+++ src/index.js
@@ -2,29 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Perf from 'react-addons-perf';
 import App from './app';
+import './vendors/fb-sdk';
 
 window.Perf = Perf;
-
-window.fbAsyncInit = () => {
-  window.FB.init({
-    appId: process.env.REACT_APP_FB_APP_ID,
-    cookie: true,
-    xfbml: true,
-    version: 'v2.8',
-  });
-  window.FB.AppEvents.logPageView();
-};
-
-((d, s, id) => {
-  const fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  const js = d.createElement(s);
-  js.id = id;
-  js.src = '//connect.facebook.net/en_US/sdk.js';
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, 'script', 'facebook-jssdk');
 
 ReactDOM.render(
   <App />,
