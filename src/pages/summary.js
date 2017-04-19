6832bd1cb53115f5f1640f645a5f8834f2933864
warizz yutanan <warizz.yutanan@gmail.com>
2017-04-19 19:25:18 +0700

    refine

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -4,79 +4,8 @@ import moment from 'moment';
 import actions from '../actions/index';
 import constants from '../constants/index';
 import service from '../services/index';
-
-const style = {
-  summary: {
-    container: {
-      backgroundColor: 'white',
-      border: '1px solid #b8bfc3',
-      borderRadius: '5px',
-      fontWeight: 'bold',
-      margin: '0 0 10px 0',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      overflow: 'hidden',
-    },
-    item: {
-      base: {
-        padding: '10px',
-      },
-      date: {
-        borderBottom: '1px solid #b8bfc3',
-        textAlign: 'center',
-      },
-    },
-  },
-  list: {
-    item: {
-      container: {
-        position: 'relative',
-        width: '300px',
-        maxWidth: '300px',
-        border: '1px solid #b8bfc3',
-        borderRadius: '5px',
-        overflow: 'hidden',
-      },
-      title: {
-        borderBottom: '1px solid #b8bfc3',
-        padding: '10px',
-        backgroundColor: 'white',
-      },
-      body: {
-        backgroundColor: 'white',
-        padding: '10px',
-      },
-    },
-  },
-  action: {
-    container: {
-      display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: 'rgb(246, 247, 249)',
-    },
-    button: {
-      width: '100%',
-      backgroundColor: 'transparent',
-      border: 'none',
-      borderTop: '1px solid rgb(184, 191, 195)',
-      padding: '10px',
-      fontWeight: 'bold',
-      color: 'rgb(148, 146, 146)',
-      cursor: 'pointer',
-    },
-  },
-};
-
-const styles = {
-  base: {
-    marginTop: '1em',
-    ...constants.elementStyle.flexContainerColumnCenter,
-  },
-  paidItem: {
-    textDecoration: 'line-through',
-  },
-};
+import Card from '../components/card';
+import './summary.css';
 
 class SummaryPage extends React.Component {
   constructor(props) {
@@ -143,7 +72,6 @@ class SummaryPage extends React.Component {
     this.props.setAlert(alertText);
     this.props.setFetching(false);
   }
-
   render() {
     const { currentPeriod } = this.props;
     const { summary } = this.state;
@@ -175,78 +103,78 @@ class SummaryPage extends React.Component {
       .reduce((a, b) => a + b, 0);
 
     return (
-      <div>
-        <div>
-          <div style={style.summary.container}>
-            <div style={{ ...style.summary.item.base, ...style.summary.item.date }}>
-              {moment(currentPeriod.endedAt).format('D MMMM YYYY')}
-            </div>
-            <div style={style.summary.item.base}>{`total: ${total} ฿`}</div>
-            <div style={style.action.container}>
-              <button style={style.action.button} onClick={this.copyToClipboard}>copy to clipboard</button>
-            </div>
+      <div className="summary-component">
+        <Card>
+          <div className="title">
+            {moment(currentPeriod.endedAt).format('D MMMM YYYY')}
           </div>
-          <div id="for-clipboard">
-            {buyers.map((buyer) => {
-              const sumPrice = buyer.bets
-                .map(service.calculation.calculateTotal(result))
-                .reduce((a, b) => a + b);
-              // check if this user paid or not
-              const paid = buyer.bets
-                .map(bet => bet.isPaid)
-                .includes(true);
-              const itemStyle = paid ? styles.paidItem : {};
-              return (
-                <div key={buyer.name} style={style.list.item.container}>
-                  <div style={style.list.item.title}>{buyer.name}</div>
-                  <div style={style.list.item.body}>
-                    <ul>
-                      {buyer.bets
-                        .map((betItem) => {
-                          const rewardCallback = (number, price, reward, rewardType) => `ถูก ${rewardType} [${number}] ${price} x ${reward} = ${price * reward} บาท`;
-                          const reward = service.calculation.checkReward(result, rewardCallback)(betItem);
-                          if (reward) {
-                            const winningItemStyle = { ...itemStyle, fontWeight: 'bold', color: '#B71C1C' };
-                            return (
-                              <li key={`bet-it--${betItem.id}`} style={winningItemStyle}>
-                                {reward}
-                              </li>
-                            );
-                          }
-                          const price1Label = betItem.number.length > 2 ? ' เต็ง ' : ' บน ';
-                          const price2Label = betItem.number.length > 2 ? ' โต๊ด ' : ' ล่าง ';
-                          const price3Label = ' ล่าง ';
-                          let bet = `${betItem.number} =`;
-                          bet += betItem.price1 ? `${price1Label}${betItem.price1}` : '';
-                          bet += betItem.price2 ? `${price2Label}${betItem.price2}` : '';
-                          bet += betItem.price3 ? `${price3Label}${betItem.price3}` : '';
+          <div className="body">
+            {`total: ${total} ฿`}
+          </div>
+          <div className="action">
+            <button onClick={this.copyToClipboard}>copy to clipboard</button>
+          </div>
+        </Card>
+        <div id="for-clipboard">
+          {buyers.map((buyer) => {
+            const sumPrice = buyer.bets
+              .map(service.calculation.calculateTotal(result))
+              .reduce((a, b) => a + b);
+            // check if this user paid or not
+            const paid = buyer.bets
+              .map(bet => bet.isPaid)
+              .includes(true);
+            // const itemStyle = paid ? styles.paidItem : {};
+            return (
+              <Card key={buyer.name}>
+                <div className="title">{buyer.name}</div>
+                <div className="body">
+                  <ul>
+                    {buyer.bets
+                      .map((betItem) => {
+                        const rewardCallback = (number, price, reward, rewardType) => `ถูก ${rewardType} [${number}] ${price} x ${reward} = ${price * reward} บาท`;
+                        const reward = service.calculation.checkReward(result, rewardCallback)(betItem);
+                        if (reward) {
+                          // const winningItemStyle = { ...itemStyle, fontWeight: 'bold', color: '#B71C1C' };
                           return (
-                            <li key={`bet-it--${betItem.id}`} style={itemStyle}>
-                              {bet}
+                            <li key={`bet-it--${betItem.id}`} className={`bet win${paid ? ' paid' : ''}`}>
+                              {reward}
                             </li>
                           );
                         }
-                      )}
-                    </ul>
-                  </div>
-                  <div style={style.action.container}>
-                    {!currentPeriod.isOpen && this.state.processingUser === buyer.name && <span style={{ fontWeight: 'bold' }}>...</span>}
-                    {!currentPeriod.isOpen && this.state.processingUser !== buyer.name && (
-                      <label htmlFor={`paid-check-for-${buyer.name}`} style={style.action.button}>
-                        <input
-                          checked={buyer.bets.map(bet => bet.isPaid).includes(true)}
-                          id={`paid-check-for-${buyer.name}`}
-                          onChange={this.setPaid(currentPeriod.id, buyer.id, !paid)}
-                          style={{ marginRight: '1em' }}
-                          type="checkbox"
-                        />
-                        {paid ? 'จ่ายแล้ว' : `ต้องจ่ายทั้งหมด: ${sumPrice} ฿`}</label>
+                        const price1Label = betItem.number.length > 2 ? ' เต็ง ' : ' บน ';
+                        const price2Label = betItem.number.length > 2 ? ' โต๊ด ' : ' ล่าง ';
+                        const price3Label = ' ล่าง ';
+                        let bet = `${betItem.number} =`;
+                        bet += betItem.price1 ? `${price1Label}${betItem.price1}` : '';
+                        bet += betItem.price2 ? `${price2Label}${betItem.price2}` : '';
+                        bet += betItem.price3 ? `${price3Label}${betItem.price3}` : '';
+                        return (
+                          <li key={`bet-it--${betItem.id}`} className={`bet${paid ? ' paid' : ''}`}>
+                            {bet}
+                          </li>
+                        );
+                      }
                     )}
-                  </div>
+                  </ul>
                 </div>
-              );
-            })}
-          </div>
+                <div className="action">
+                  {!currentPeriod.isOpen && this.state.processingUser === buyer.name && <span style={{ fontWeight: 'bold' }}>...</span>}
+                  {!currentPeriod.isOpen && this.state.processingUser !== buyer.name && (
+                    <label htmlFor={`paid-check-for-${buyer.name}`}>
+                      <input
+                        checked={buyer.bets.map(bet => bet.isPaid).includes(true)}
+                        id={`paid-check-for-${buyer.name}`}
+                        onChange={this.setPaid(currentPeriod.id, buyer.id, !paid)}
+                        style={{ marginRight: '1em' }}
+                        type="checkbox"
+                      />
+                      {paid ? 'จ่ายแล้ว' : `ต้องจ่ายทั้งหมด: ${sumPrice} ฿`}</label>
+                  )}
+                </div>
+              </Card>
+            );
+          })}
         </div>
       </div>
     );
