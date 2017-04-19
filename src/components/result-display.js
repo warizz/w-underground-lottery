28d7e8699abf0d26ebcbc7e157333fa7259b5794
warizz yutanan <warizz.yutanan@gmail.com>
2017-04-19 14:53:25 +0700

    to .css and refine to new design

--- src/components/result-display.js
+++ src/components/result-display.js
@@ -1,55 +1,32 @@
 import React, { PropTypes } from 'react';
-
-const styles = {
-  base: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: '1em',
-  },
-  six: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    fontWeight: 'bold',
-  },
-  two: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    fontWeight: 'bold',
-  },
-  three: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    width: '100%',
-  },
-};
+import './result-display.css';
 
 const ResultDisplay = props => (
-  <div style={styles.base}>
-    <h2>ผลรางวัลงวด</h2>
-    <h1 style={{ fontWeight: '700' }}>{props.endedAt}</h1>
-    <section style={styles.six}>
-      <h2>รางวัลที่หนึ่ง</h2>
-      <span style={{ fontSize: '50px' }}>{props.six}</span>
-    </section>
-    <section style={styles.two}>
-      <h3>เลขท้ายสองตัว</h3>
-      <span style={{ fontSize: '30px' }}>{props.two}</span>
-    </section>
-    <section style={styles.three}>
-      <h3>สามตัวล่าง</h3>
-      <div style={{ marginRight: '1em', width: '50%', display: 'flex', justifyContent: 'space-between' }}>
-        <span style={{ fontSize: '30px' }}>{props.firstThree}</span>
-        <span style={{ fontSize: '30px' }}>{props.secondThree}</span>
+  <div className="result-display">
+    <div className="title">{props.endedAt}</div>
+    <div className="body">
+      <div className="row">
+        <div className="result-group">
+          <h2>รางวัลที่หนึ่ง</h2>
+          <span style={{ fontSize: '50px' }}>{props.six}</span>
+        </div>
       </div>
-    </section>
+      <div className="row">
+        <div className="result-group">
+          <h3>เลขท้ายสองตัว</h3>
+          <span style={{ fontSize: '30px' }}>{props.two}</span>
+        </div>
+      </div>
+      <div className="row">
+        <div className="result-group">
+          <h3>สามตัวล่าง</h3>
+          <div className="three">
+            <span style={{ fontSize: '30px' }}>{props.firstThree}</span>
+            <span style={{ fontSize: '30px' }}>{props.secondThree}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
