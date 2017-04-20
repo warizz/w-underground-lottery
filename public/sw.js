2850ca8b089dca2e66d2ac0a7e3297e99bbb2422
warizz yutanan <warizz.yutanan@gmail.com>
2017-04-20 13:12:34 +0700

    4.3.2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '4.3.1';
+const CACHE_NAME = '4.3.2';
 const urlsToCache = [
   './',
 ];
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const discountPercent = 0.1; // 1 means 100%
-export const appVersion = '4.3.1';
+export const appVersion = '4.3.2';
