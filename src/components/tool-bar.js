f8414982557fbf4189b026028c8656e4de78b251
warizz <warizz.yutanan@gmail.com>
2017-06-04 22:18:48 +0700

    Create ToolBar's unit tests

--- src/components/tool-bar.js
+++ src/components/tool-bar.js
@@ -2,24 +2,15 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import './tool-bar.css';
 
-class ToolBar extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return nextProps.pageName !== this.props.pageName;
-  }
-  render() {
-    const { pageName } = this.props;
-    return (
-      <div className="tool-bar">
-        {pageName === 'Home' && <i className="material-icons">home</i>}
-        {pageName !== 'Home' &&
-          <button className="icon-button" onClick={() => window.history.back()}>
-            <i className="material-icons">keyboard_backspace</i>
-          </button>}
-        <span className="page-name">{pageName}</span>
-      </div>
-    );
-  }
-}
+const ToolBar = ({ pageName }) =>
+  <div className="tool-bar">
+    {pageName === 'Home' && <i className="material-icons">home</i>}
+    {pageName !== 'Home' &&
+      <button className="icon-button" onClick={window.history.back}>
+        <i className="material-icons">keyboard_backspace</i>
+      </button>}
+    {pageName && <span className="page-name">{pageName}</span>}
+  </div>;
 
 ToolBar.propTypes = {
   pageName: PropTypes.string,
