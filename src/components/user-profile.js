a9feeb1fcda599b989fec7817fc256a546e684da
warizz <warizz.yutanan@gmail.com>
2017-06-05 23:12:58 +0700

    Add unit test for component/user-profile

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -4,36 +4,58 @@ import Link from 'react-router/lib/Link';
 import Card from './card';
 import './user-profile.css';
 
-const UserProfile = props => (
-  <div className="user-profile">
-    {!props.user && (
-      <Card>
-        <div className="placeholder">{'...'}</div>
-      </Card>
-    )}
-    {props.user && (
+const UserProfile = (props) => {
+  let body;
+
+  if (props.user) {
+    let dashboardLink;
+
+    if (props.user.is_admin) {
+      dashboardLink = <Link to="/dashboard" className="border-bottom">go to admin dashboard</Link>;
+    } else {
+      dashboardLink = null;
+    }
+
+    body = (
       <Card>
         <div className="body center">
           <img className="picture" src={props.user.picture} alt="user-profile" role="presentation" />
           <div className="name">{props.user.name}</div>
         </div>
         <div className="action column">
-          {props.user.is_admin && <Link to="/dashboard" className="border-bottom">go to admin dashboard</Link>}
+          {dashboardLink}
           <Link to="/history" className="border-bottom">see history</Link>
           <button onClick={props.logOutHandler}>log out</button>
         </div>
       </Card>
-    )}
-  </div>
-);
+    );
+  } else {
+    body = (
+      <Card>
+        <div className="placeholder">{'...'}</div>
+      </Card>
+    );
+  }
+
+  return (
+    <div className="user-profile">
+      {body}
+    </div>
+  );
+};
 
 UserProfile.propTypes = {
   logOutHandler: PropTypes.func.isRequired,
   user: PropTypes.shape({
-    is_admin: PropTypes.bool,
-    name: PropTypes.string,
-    picture: PropTypes.string,
+    is_admin: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
   }),
+};
+
+UserProfile.defaultProps = {
+  logOutHandler() {},
+  user: null,
 };
 
 export default UserProfile;
