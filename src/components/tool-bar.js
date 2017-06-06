21468b405bc4c4993f84b83bbf82d31a9aefcf81
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-06 13:06:30 +0700

    Refactor component/tool-bar

--- src/components/tool-bar.js
+++ src/components/tool-bar.js
@@ -2,21 +2,44 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import './tool-bar.css';
 
-const ToolBar = ({ pageName }) =>
-  (<div className="tool-bar">
-    {pageName === 'Home' && <i className="material-icons">home</i>}
-    {pageName !== 'Home' &&
-      <button className="icon-button" onClick={() => window.history.back()}>
+const ToolBar = (props) => {
+  const { backButtonClickedCallback, pageName } = props;
+  let elementIcon = null;
+  let elementBackButton = null;
+  let elementPageName = null;
+
+  if (pageName && pageName.toLowerCase() === 'home') {
+    elementIcon = <i className="material-icons">home</i>;
+  }
+
+  if (!pageName || pageName.toLowerCase() !== 'home') {
+    elementBackButton = (
+      <button className="icon-button back" onClick={backButtonClickedCallback}>
         <i className="material-icons">keyboard_backspace</i>
-      </button>}
-    {pageName && <span className="page-name">{pageName}</span>}
-  </div>);
+      </button>
+    );
+  }
+
+  if (pageName) {
+    elementPageName = <span className="page-name">{pageName}</span>;
+  }
+
+  return (
+    <div className="tool-bar">
+      {elementIcon}
+      {elementBackButton}
+      {elementPageName}
+    </div>
+  );
+};
 
 ToolBar.propTypes = {
+  backButtonClickedCallback: PropTypes.func,
   pageName: PropTypes.string,
 };
 
 ToolBar.defaultProps = {
+  backButtonClickedCallback() {},
   pageName: null,
 };
 
