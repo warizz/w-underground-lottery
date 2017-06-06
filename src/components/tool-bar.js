1594ddb1fdc06379222a7b1a3b953fe8d4f188d7
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-06 12:35:52 +0700

    Fix linter issues

--- src/components/tool-bar.js
+++ src/components/tool-bar.js
@@ -3,17 +3,21 @@ import { PropTypes } from 'prop-types';
 import './tool-bar.css';
 
 const ToolBar = ({ pageName }) =>
-  <div className="tool-bar">
+  (<div className="tool-bar">
     {pageName === 'Home' && <i className="material-icons">home</i>}
     {pageName !== 'Home' &&
       <button className="icon-button" onClick={window.history.back}>
         <i className="material-icons">keyboard_backspace</i>
       </button>}
     {pageName && <span className="page-name">{pageName}</span>}
-  </div>;
+  </div>);
 
 ToolBar.propTypes = {
   pageName: PropTypes.string,
+};
+
+ToolBar.defaultProps = {
+  pageName: null,
 };
 
 export default ToolBar;
