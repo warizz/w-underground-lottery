f5a98e99006a55397528daa25d1ddbde0294642e
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-07 18:24:56 +0700

    Fix wrong unit test file name

---
+++ src/__tests__/container/dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { DashboardContainer } from '../../container/dashboard';
+
+it('should render contained component', () => {
+  const wrapper = mount(<DashboardContainer />);
+
+  expect(wrapper.find('div.dashboard').exists()).toBe(true);
+});
+
+it('should udpate state.endDate when call onEndDateChange()', () => {
+  const wrapper = mount(<DashboardContainer />);
+  const endDate = new Date();
+
+  wrapper.instance().onEndDateChange({ target: { value: endDate } });
+  expect(wrapper.state('endDate').getTime()).toBe(endDate.getTime());
+});
