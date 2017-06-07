68c7bc63023ab58b991e3828a5817dbc700c7f21
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-07 19:30:08 +0700

    Unit test for closePeriod() (WIP)

--- src/__tests__/container/dashboard.js
+++ src/__tests__/container/dashboard.js
@@ -15,3 +15,26 @@ it('should update state.endDate when call onEndDateChange()', () => {
   wrapper.instance().onEndDateChange({ target: { value: endDate } });
   expect(wrapper.state('endDate').getTime()).toBe(endDate.getTime());
 });
+
+it('should call service.data.updatePeriod then call setPeriod when call closePeriod()', () => {
+  const setCurrentPeriodMock = jest.fn();
+  const props = {
+    setFetching() {},
+    setCurrentPeriod: setCurrentPeriodMock,
+    service: {
+      data: {
+        getCurrentPeriod() {
+          return new Promise(resolve => resolve());
+        },
+        openPeriod() {},
+        updatePeriod() {
+          return new Promise(resolve => resolve());
+        },
+      },
+    },
+  };
+  const wrapper = mount(<DashboardContainer {...props} />);
+
+  wrapper.instance().closePeriod();
+  // expect(setCurrentPeriodMock).toHaveBeenCalled();
+});
