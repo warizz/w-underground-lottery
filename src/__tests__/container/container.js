68dfe01126f9c3f8e571b38d264bc00b626cb36e
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-07 17:08:49 +0700

    Add test for container/dashboard

---
+++ src/__tests__/container/container.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Container from '../../container/dashboard';
+import reducer from '../../reducers/index';
+
+const store = createStore(reducer);
+
+it('should render contained component', () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <Container />
+    </Provider>,
+  );
+
+  expect(wrapper.find('div.dashboard').exists()).toBe(true);
+});
