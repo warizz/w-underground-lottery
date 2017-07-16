e7aab4b5c37bfe9e05e36d7928212b7003f0b76b
warizz yutanan <warizz.yutanan@gmail.com>
2017-07-16 17:44:38 +0700

    Update sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v5';
+const CACHE_NAME = 'v6';
 const urlsToCache = ['./', 'https://fonts.googleapis.com/icon?family=Material+Icons'];
 
 this.addEventListener('install', (event) => {
