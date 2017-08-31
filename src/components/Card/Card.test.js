74123f58f9479a15745766159ee52e3184f7e94d
warizz yutanan <warizz.yutanan@gmail.com>
2017-08-31 23:23:22 +0700

    Unit test Card

---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Sut from './index';
+
+it('should render children', () => {
+  const wrapper = shallow(
+    <Sut>
+      <div className="test-element">test-text</div>
+    </Sut>
+  );
+
+  expect(wrapper.find('div.card-component').exists()).toBe(true);
+  expect(wrapper.find('div.test-element').exists()).toBe(true);
+  expect(wrapper.find('div.test-element').text()).toBe('test-text');
+});
