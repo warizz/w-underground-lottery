9e419fe3ded5a3713935d14dcd5ba940d4304859
Warizz Yutanan <warizz.yutanan@gmail.com>
2018-02-18 12:46:05 +0700

    refactor lib with proxy

--- src/constants/lib.js
+++ src/constants/lib.js
@@ -1,27 +1,34 @@
 import action from '../actions/index';
-import service from '../services/index';
+import proxy from '../proxy';
 
 function initApplicationState(store, cookieManager) {
-  return (nextState, replace, callback) => {
+  return async (nextState, replace, callback) => {
     store.dispatch(action.data.setFetching(true));
-    Promise.all([service.data.getUser(), service.data.getCurrentPeriod()])
-      .then(values => {
-        const [user, currentPeriod] = values;
-        store.dispatch(action.user.setUser(user));
-        store.dispatch(action.data.setCurrentPeriod(currentPeriod));
-        store.dispatch(action.data.setFetching(false));
-        callback();
-      })
-      .catch(error => {
-        if (error.response) {
-          if (error.response.status === 401) {
-            cookieManager.removeToken();
-            replace('/log-in');
-          }
+
+    try {
+      const values = await Promise.all([
+        proxy.getUser(),
+        proxy.getCurrentPeriod(),
+      ]);
+
+      const [user, currentPeriod] = values;
+
+      store.dispatch(action.user.setUser(user));
+      store.dispatch(action.data.setCurrentPeriod(currentPeriod));
+      store.dispatch(action.data.setFetching(false));
+
+      callback();
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          cookieManager.removeToken();
+          replace('/log-in');
         }
-        store.dispatch(action.data.setFetching(false));
-        callback();
-      });
+      }
+    } finally {
+      store.dispatch(action.data.setFetching(false));
+      callback();
+    }
   };
 }
 
